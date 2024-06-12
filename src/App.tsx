232f589cf3317cb6f7a0620cd6541469727a249b
Bernard Namangala <58230307+Bernard-Namangala@users.noreply.github.com>
2024-06-13 00:49:58 +0200

    refresh logged in user after login

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,17 @@ import { AppLayout } from "@/components";
 import { Home, MyAccount, Nfts } from "@/pages";
 import "@/styles/App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { UserProvider } from "./contexts/UserContext";
 
 function App() {
   return (
     <BrowserRouter>
       <AppLayout>
-        <UserProvider>
-          <Routes>
-            <Route path="*" element={<Home />} />
-            <Route path="nfts" element={<Nfts />} />
-            <Route path="myAccount" element={<MyAccount />} />
-          </Routes>
-        </UserProvider>
+        x
+        <Routes>
+          <Route path="*" element={<Home />} />
+          <Route path="nfts" element={<Nfts />} />
+          <Route path="myAccount" element={<MyAccount />} />
+        </Routes>
       </AppLayout>
     </BrowserRouter>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -5,24 +5,27 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { PrivyProvider } from "@privy-io/react-auth";
 import logo from "@/assets/logo.png";
+import { UserProvider } from "./contexts/UserContext";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <AppProvider>
-      <PrivyProvider
-        appId="clwywj9cw03rbz15b6nadav0u"
-        config={{
-          loginMethods: ["twitter", "wallet"],
+      <UserProvider>
+        <PrivyProvider
+          appId="clwywj9cw03rbz15b6nadav0u"
+          config={{
+            loginMethods: ["twitter", "wallet"],
 
-          // Customize Privy's appearance in  app
-          appearance: {
-            theme: "dark",
-            logo: logo,
-          },
-        }}
-      >
-        <App />
-      </PrivyProvider>
+            // Customize Privy's appearance in  app
+            appearance: {
+              theme: "dark",
+              logo: logo,
+            },
+          }}
+        >
+          <App />
+        </PrivyProvider>
+      </UserProvider>
     </AppProvider>
   </React.StrictMode>
 );
