bb15e41c1b4b67f4dd7e416f2ee755af111c8711
Muhammad Auzair <78294230+Nobody12here@users.noreply.github.com>
2024-07-19 21:31:04 +0500

    remove unnessary stuff

--- src/pages/Staking.tsx
+++ src/pages/Staking.tsx
@@ -1,12 +1,8 @@
 import StakingHero from '@/components/Staking/StakingHero';
 import StakingInfo from '@/components/Staking/StakingInfo';
-import {calculateYeildPoint} from '@/utils/staking';
 import {Row} from 'antd';
-import {useAccount} from 'wagmi';
 
 const Staking = () => {
-  const {address} = useAccount();
-  calculateYeildPoint(address);
   return (
     <Row>
       <StakingHero />
